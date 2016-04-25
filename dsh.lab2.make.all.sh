9f43ab31c62e31005926d22be3e53596a5681bf7
August Valera <4U6U57@gmail.com>
2016-04-25 13:23:06 -0700

    Got filename in make, not yet done

--- dsh.lab2.make.all.sh
+++ dsh.lab2.make.all.sh
@@ -2,10 +2,34 @@
 
 ASG="lab2"
 DFILE=".d.make.f"
+BACKUP=".backup"
 
-rm -f $DFILE
+rm -rf $DFILE $BACKUP/
+mkdir $BACKUP
+cp * $BACKUP
+
+JAVASRC=""
+for FILE in *; do
+	if [[ $FILE == *everse* ]]; then
+		if [[ ! -e FileReverse.java ]]; then
+			JAVASRC=$FILE
+		else
+			JAVASRC=FileReverse.java
+		fi
+	fi
+done
+echo "$JAVASRC"
+
 if [[ -e Makefile ]]; then
 	echo "5 / 5 | Makefile exists" >> $DFILE
 else 
-	echo "0 / 5 | No Makefile" >> $DFILE
+	echo "0 / 5 | No Makefile (ls: $(ls -m))" >> $DFILE
 fi
+
+cd $BACKUP
+for FILE in *; do
+	if [[ ! -e ../$FILE ]]; then
+		echo "X / X | WARNING! Your Makefile deletes $FILE" >> $DFILE
+		cp $FILE ..
+	fi
+done
